2888b6f3966b13ee42b8df3d9128222184a2e3bb
ritik jha <coolritikjha46@gmail.com>
2023-02-03 11:07:13 +0530

    changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv')
 const fileUpload = require('express-fileupload')
 const connectToMongo = require('./db/db')
 const {readdirSync} = require('fs')
+const userRoutes = require('./routes/userRoutes')
+const uploadRoutes = require('./routes/uploadRoutes')
+const postRoutes = require('./routes/postRoutes')
 const PORT = 5000
 dotenv.config()
 const app = express()
@@ -17,8 +20,9 @@ app.use(fileUpload({
     useTempFiles : true
 }))
 connectToMongo()
-readdirSync('./routes').map((r)=>app.use('/',require("./routes/" + r)))
-console.log((new Date() * Math.random()).toString().substring(0,1))
+app.use('/',userRoutes)
+app.use('/',uploadRoutes)
+app.use('/',postRoutes)
 
 app.listen(process.env.PORT || PORT,()=>{
     console.log('server connected succesfully');
